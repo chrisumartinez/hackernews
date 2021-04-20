c2bfabd419c0ee7962a6497505c2d3069e9c5898
chrisumartinez <chrisumartinez@gmail.com>
2021-04-20 01:26:55 -0700

    added css styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import "./App.css";
 
 const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
 //custom hook:
@@ -102,7 +103,7 @@ const App = () => {
 
 	const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => {
 		return (
-			<form onSubmit={onSearchSubmit}>
+			<form onSubmit={onSearchSubmit} className="search-form">
 				<InputWithLabel
 					label="search"
 					value={searchTerm}
@@ -111,7 +112,11 @@ const App = () => {
 				>
 					<strong>Search:</strong>
 				</InputWithLabel>
-				<button type="submit" disabled={!searchTerm}>
+				<button
+					type="submit"
+					className="button button_large"
+					disabled={!searchTerm}
+				>
 					Submit
 				</button>
 			</form>
@@ -154,8 +159,8 @@ const App = () => {
 	};
 
 	return (
-		<div>
-			<h1>My Hacker Stories</h1>
+		<div className="container">
+			<h1 className="headline-primary">My Hacker Stories</h1>
 			<SearchForm
 				searchTerm={searchTerm}
 				onSearchInput={handleSearchInput}
@@ -192,7 +197,9 @@ const InputWithLabel = ({
 
 	return (
 		<>
-			<label htmlFor={id}>{children}</label>
+			<label htmlFor={id} className="label">
+				{children}
+			</label>
 			&nbsp;
 			<input
 				id={id}
@@ -200,6 +207,7 @@ const InputWithLabel = ({
 				value={value}
 				onChange={onInputChange}
 				ref={inputRef}
+				className="input"
 			/>
 		</>
 	);
@@ -212,15 +220,19 @@ const List = ({ list, onRemoveItem }) =>
 
 const Item = ({ item, onRemoveItem }) => {
 	return (
-		<div>
-			<span>
+		<div className="item">
+			<span style={{ width: "40%" }}>
 				<a href={item.url}>{item.title}</a>
 			</span>
-			<span>{item.author}</span>
-			<span>{item.num_comments}</span>
-			<span>{item.points}</span>
+			<span style={{ width: "30%" }}>{item.author}</span>
+			<span style={{ width: "10%" }}>{item.num_comments}</span>
+			<span style={{ width: "10%" }}>{item.points}</span>
 			<span>
-				<button type="button" onClick={() => onRemoveItem(item)}>
+				<button
+					type="button"
+					className="button button_small"
+					onClick={() => onRemoveItem(item)}
+				>
 					Dismiss
 				</button>
 			</span>
