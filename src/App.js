00df3466850ca03445a19f564d8d723f62f30c95
chrisumartinez <chrisumartinez@gmail.com>
2021-04-21 00:52:37 -0700

    used styled components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,88 @@
 import React from "react";
 import axios from "axios";
 import styles from "./App.module.css";
+import styled from "styled-components";
+/*
+CSS in JS
+*/
+const StyledContainer = styled.div`
+	height: 100vw;
+	padding: 20px;
+
+	background: #83a4d4;
+	background: linear-gradient(to left, #b6fbff, #83a4d4);
+
+	color: #171212;
+`;
+
+const StyledHeadlinePrimary = styled.h1`
+	font-size: 48px;
+	font-weight: 300;
+	letter-spacing: 2px;
+`;
+
+const StyledItem = styled.div`
+	display: flex;
+	align-items: center;
+	padding-bottom: 5px;
+`;
+
+const StyledColumn = styled.span`
+	padding: 0 5px;
+	white-space: nowrap;
+	overflow: hidden;
+	white-space: nowrap;
+	text-overflow: ellipsis;
+
+	a {
+		color: inherit;
+	}
+
+	width: ${(props) => props.width};
+`;
+
+const StyledButton = styled.button`
+	background: transparent;
+	border: 1px solid #171212;
+	padding: 5px;
+	cursor: pointer;
+
+	transition: all 0.1s ease-in;
+
+	&:hover {
+		background: #171212;
+		color: #ffffff;
+	}
+`;
+
+const StyledButtonSmall = styled(StyledButton)`
+	padding: 5px;
+`;
+
+const StyledButtonLarge = styled(StyledButton)`
+	padding: 10px;
+`;
+
+const StyledSearchForm = styled.form`
+	padding: 10px 0 20px 0;
+	display: flex;
+	align-items: baseline;
+`;
+
+const StyledLabel = styled.label`
+	border-top: 1px solid #171212;
+	border-left: 1px solid #171212;
+	padding-left: 5px;
+	font-size: 24px;
+`;
+
+const StyledInput = styled.input`
+	border: none;
+	border-bottom: 1px solid #171212;
+	background-color: transparent;
+
+	font-size: 24px;
+`;
 
 const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
 //custom hook:
@@ -103,7 +185,7 @@ const App = () => {
 
 	const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => {
 		return (
-			<form onSubmit={onSearchSubmit} className={styles.searchForm}>
+			<StyledSearchForm onSubmit={handleSearchSubmit}>
 				<InputWithLabel
 					label="search"
 					value={searchTerm}
@@ -112,14 +194,10 @@ const App = () => {
 				>
 					<strong>Search:</strong>
 				</InputWithLabel>
-				<button
-					type="submit"
-					className={`${styles.button}${styles.buttonLarge}`}
-					disabled={!searchTerm}
-				>
+				<StyledButtonLarge type="submit" disabled={!searchTerm}>
 					Submit
-				</button>
-			</form>
+				</StyledButtonLarge>
+			</StyledSearchForm>
 		);
 	};
 
@@ -159,8 +237,8 @@ const App = () => {
 	};
 
 	return (
-		<div className={styles.container}>
-			<h1 className={styles.headlinePrimary}>My Hacker Stories</h1>
+		<StyledContainer>
+			<StyledHeadlinePrimary>My Hacker Stories</StyledHeadlinePrimary>
 			<SearchForm
 				searchTerm={searchTerm}
 				onSearchInput={handleSearchInput}
@@ -175,7 +253,7 @@ const App = () => {
 			)}
 
 			<hr />
-		</div>
+		</StyledContainer>
 	);
 };
 
@@ -197,11 +275,9 @@ const InputWithLabel = ({
 
 	return (
 		<>
-			<label htmlFor={id} className={styles.label}>
-				{children}
-			</label>
+			<StyledLabel htmlFor={id}>{children}</StyledLabel>
 			&nbsp;
-			<input
+			<StyledInput
 				id={id}
 				type={type}
 				value={value}
@@ -220,23 +296,22 @@ const List = ({ list, onRemoveItem }) =>
 
 const Item = ({ item, onRemoveItem }) => {
 	return (
-		<div className={styles.item}>
-			<span style={{ width: "40%" }}>
+		<StyledItem>
+			<StyledColumn width="40%">
 				<a href={item.url}>{item.title}</a>
-			</span>
-			<span style={{ width: "30%" }}>{item.author}</span>
-			<span style={{ width: "10%" }}>{item.num_comments}</span>
-			<span style={{ width: "10%" }}>{item.points}</span>
-			<span>
-				<button
+			</StyledColumn>
+			<StyledColumn width="30%">{item.author}</StyledColumn>
+			<StyledColumn width="10%">{item.num_comments}</StyledColumn>
+			<StyledColumn width="10%">{item.points}</StyledColumn>
+			<StyledColumn width="10%">
+				<StyledButtonSmall
 					type="button"
-					className={`${styles.button} ${styles.buttonSmall}`}
 					onClick={() => onRemoveItem(item)}
 				>
 					Dismiss
-				</button>
-			</span>
-		</div>
+				</StyledButtonSmall>
+			</StyledColumn>
+		</StyledItem>
 	);
 };
 
