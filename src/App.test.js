a0d187e00cec1384ab4f6587cc078f0d7fe43c3d
chrisumartinez <chrisumartinez@gmail.com>
2021-04-26 08:40:15 -0700

    refactored styledComponents, added testing functions

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+// Example of a test suite with tests:
+//Describe block is test suite, test blocks are test cases:
+describe("something truthy and falsy", () => {
+	test("true to be true", () => {
+		expect(true).toBeTruthy();
+	});
+	test("false to be false", () => {
+		expect(false).toBeFalsy();
+	});
 });
+
+//
