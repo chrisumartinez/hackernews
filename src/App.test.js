0a96f1be9bbcc65f2e99590a6b6495ce83fc50a6
chrisumartinez <chrisumartinez@gmail.com>
2021-05-01 12:12:04 -0700

    added snapshot testing, finished testing

--- src/App.test.js
+++ src/App.test.js
@@ -173,6 +173,11 @@ describe("SearchForm", () => {
 		fireEvent.submit(screen.getByRole("button"));
 		expect(searchFormProps.onSearchSubmit).toHaveBeenCalledTimes(1);
 	});
+
+	test("renders snapshot", () => {
+		const { container } = render(<SearchForm {...searchFormProps} />);
+		expect(container.firstChild).toMatchSnapshot();
+	});
 });
 
 //Testing List Component:
